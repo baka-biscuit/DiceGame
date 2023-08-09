9e7d2d1120e17b8526dd8cc9b09a70ffa9f4978a
baka-biscuit <141296954+baka-biscuit@users.noreply.github.com>
2023-08-10 00:09:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var players = document.querySelectorAll("img");
+var randomList = [];
+var winnerText = document.getElementById("winner");
+for(i=0;i<players.length;i++){
+    var randomNum = GetRandomDice()
+    randomList.push(randomNum);
+    players[i].setAttribute("src", "./images/dice"+randomNum+".png");
+}
+
+if(randomList[0] === randomList[1]){
+    winnerText.textContent = "Draw";
+}
+else if(randomList[0] > randomList[1]){
+    winnerText.textContent = "Player1 is the winner!!.";
+}
+else{
+    winnerText.textContent = "Player2 is the winner!!.";
+}
+
+function GetRandomDice(){
+    var randomNumber = Math.random();
+    var randomNumber = randomNumber * 6;
+    randomNumber = Math.floor(randomNumber)
+    if(randomNumber === 0){
+        randomNumber = 1;
+    }
+    return randomNumber;
+}
